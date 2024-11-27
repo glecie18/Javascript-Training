79fdb094c9af37c90edbfb9dbaa53ff0fe63c205
Glecie Hernandez <153145585+glecie18@users.noreply.github.com>
2024-11-27 07:12:41 +0000

    JS array code challenge

---
+++ Array methods/script.js
@@ -0,0 +1,9 @@
+const planets = ['Earth', 'Mars', 'Saturn', 'Mercury', 'Jupiter', 'Venus', 'Uranus', 'Neptune'];
+
+const planetsString = planets.join(', ');
+
+console.log(planetsString); // Outputs: 'Earth, Mars, Saturn, Mercury, Jupiter, Venus, Uranus, Neptune'
+
+const saturnIndex = planets.indexOf('Saturn');
+
+console.log(saturnIndex); // Outputs: 2
