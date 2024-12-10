876820f6fdff4f513ec22d62a5bf18b30a3048b8
Glecie Hernandez <153145585+glecie18@users.noreply.github.com>
2024-12-10 04:20:04 +0000

    node code challenge

---
+++ handling routes view/index.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var routes = require("./routes");
+
+http.createServer(function(request, response) {
+    
+    if(request.url == "/") {
+        routes.root(request, response);
+    } else if(request.url == "/contact") {
+        routes.contact(request, response);
+    } else if(request.url == "/about") {
+        routes.about(request, response);
+    } else {
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.end("Page not found\n");
+    }
+
+}).listen(8080, function() {
+    console.log("Server is listening on port 8080...");
+});
---
+++ simply merge/index.js
@@ -0,0 +1,9 @@
+var utilities = require("./utilities");
+
+var mailValues = {};
+
+mailValues.first_name = "Glecie";
+
+var emailTemplate = "Hi %first_name%! Thanks for completing this code challenge :)";
+
+var mergedContent = utilities.merge(emailTemplate, mailValues);
