91e69baefd5d97b41395fb0151de6ca02e4c48aa
Glecie Hernandez <153145585+glecie18@users.noreply.github.com>
2025-01-21 11:54:12 +0800

    Update select by tag name/apps.js
    Co-authored-by:  Arvin Viar <77320674+arvin-viar@users.noreply.github.com>

--- select by tag name/apps.js
+++ select by tag name/apps.js
@@ -2,6 +2,6 @@
 const colors = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
 const listItems = document.getElementsByTagName('li'); 
 
-for (const i = 0; i < listItems.length; i++) {
+for (let i = 0; i < listItems.length; i++) {
   listItems[i].style.color = colors[i]; 
 }
